4eb314690b1f7e02025f24e169be2ba1955e6473
wahnishjorge <wahnishjorge@wahnishjorge-EC1>
2014-05-06 20:18:09 -0300

    reverting gitignore

---
+++ src/tprestaurant/model/Carga.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tprestaurant.model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author wahnishjorge
+ */
+public class Carga {
+    
+    public void cargaIngredientes(String ingredientes, Restaurant rest){
+        File archivo = new File("c:\\" + ingredientes + ".txt");
+        TreeSet<Ingrediente> listIngredientes = new TreeSet<Ingrediente>();
+        try
+        {
+            BufferedReader in = new BufferedReader(new FileReader(archivo));
+            String data;
+            String linea[] = new String[7];
+            while((data=in.readLine())!=null)
+            {
+                Ingrediente ingr = new Ingrediente();
+                linea = data.split(",");
+                ingr.setDescripcion(linea[0]);
+                ingr.setUnidad(UnidadesDeMedida.valueOf(linea[1]));
+                ingr.setCostoPorUnidad(Float.parseFloat(linea[2]));
+                listIngredientes.add(ingr);
+            }
+            rest.setIngredientes(listIngredientes);
+         in.close();
+        }
+        catch(IOException e)
+        {
+           JOptionPane.showMessageDialog(null,"El archivo no se ha encontrado");
+        }
+       
+    
+    }
+}
--- src/tprestaurant/model/Restaurant.java
+++ src/tprestaurant/model/Restaurant.java
@@ -2,6 +2,7 @@ package tprestaurant.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 /**
  *
@@ -10,19 +11,61 @@ import java.util.ArrayList;
 public class Restaurant implements Serializable{
     
     private ArrayList<Menu> menus;
-    private ArrayList<Producto> productos;
-    private ArrayList<Ingrediente> ingredientes;
+    private TreeSet<Producto> productos;
+    private TreeSet<Ingrediente> ingredientes;
     
     public Restaurant()
     {
         menus = new ArrayList<Menu>();
-        productos = new ArrayList<Producto>();
-        ingredientes = new ArrayList<Ingrediente>();
+        productos = new TreeSet<Producto>();
+        ingredientes = new TreeSet<Ingrediente>();
     }
 
-    public Restaurant(ArrayList<Menu> menus, ArrayList<Producto> productos, ArrayList<Ingrediente> ingredientes) {
+    public Restaurant(ArrayList<Menu> menus, TreeSet<Producto> productos, TreeSet<Ingrediente> ingredientes) {
         this.menus = menus;
         this.productos = productos;
+        this.ingredientes = ingredientes;
+    }
+
+    /**
+     * @return the menus
+     */
+    public ArrayList<Menu> getMenus() {
+        return menus;
+    }
+
+    /**
+     * @param menus the menus to set
+     */
+    public void setMenus(ArrayList<Menu> menus) {
+        this.menus = menus;
+    }
+
+    /**
+     * @return the productos
+     */
+    public TreeSet<Producto> getProductos() {
+        return productos;
+    }
+
+    /**
+     * @param productos the productos to set
+     */
+    public void setProductos(TreeSet<Producto> productos) {
+        this.productos = productos;
+    }
+
+    /**
+     * @return the ingredientes
+     */
+    public TreeSet<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    /**
+     * @param ingredientes the ingredientes to set
+     */
+    public void setIngredientes(TreeSet<Ingrediente> ingredientes) {
         this.ingredientes = ingredientes;
     }
 }
