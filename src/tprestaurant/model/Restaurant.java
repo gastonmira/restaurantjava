44634055c8b6e38bb1c756ab54d11cf6aa7bbaa3
jmdapice <pepe@gmail.com>
2014-05-06 20:07:14 -0300

    changed to TreeSet

--- src/tprestaurant/model/Restaurant.java
+++ src/tprestaurant/model/Restaurant.java
@@ -2,6 +2,7 @@ package tprestaurant.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 /**
  *
@@ -10,17 +11,17 @@ import java.util.ArrayList;
 public class Restaurant implements Serializable{
     
     private ArrayList<Menu> menus;
-    private ArrayList<Producto> productos;
-    private ArrayList<Ingrediente> ingredientes;
+    private TreeSet<Producto> productos;
+    private TreeSet<Ingrediente> ingredientes;
     
     public Restaurant()
     {
         menus = new ArrayList<Menu>();
-        productos = new ArrayList<Producto>();
-        ingredientes = new ArrayList<Ingrediente>();
+        productos = new TreeSet<Producto>();
+        ingredientes = new TreeSet<Ingrediente>();
     }
 
-    public Restaurant(ArrayList<Menu> menus, ArrayList<Producto> productos, ArrayList<Ingrediente> ingredientes) {
+    public Restaurant(ArrayList<Menu> menus, TreeSet<Producto> productos, TreeSet<Ingrediente> ingredientes) {
         this.menus = menus;
         this.productos = productos;
         this.ingredientes = ingredientes;
